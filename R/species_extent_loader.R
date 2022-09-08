406f44d060df18dd8c5468fe955278b1ed0d3d20
Amira.Burns <burnsal@rams.colostate.edu>
2022-09-08 13:04:39 -0600

    add species.extent.loader csv file and function

---
+++ R/species_extent_loader.R
@@ -0,0 +1,20 @@
+#' Load data describing geographic extent of species presence
+#' 
+#' @return
+#' 
+#' @export
+#' 
+#' 
+species_extent_loader <- function(){
+  
+  #--Download Species Extent Data
+  data.path <- download_cache(url="https://raw.githubusercontent.com/CameraTrapDetectoR/CameraTrapDetectoR/main/Data/species.extent.data.csv")
+  
+  #--Read Species Extent Data
+  extent.data<-read.csv(data.path, stringsAsFactors=TRUE)
+  
+  #extent.data<-readr::read_csv(data.path, show_col_types=FALSE, progress=FALSE)
+  #extent.data<-as.data.frame(extent.data)
+  
+  return(extent.data)
+}#END Function
