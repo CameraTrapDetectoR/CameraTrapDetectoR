9892ebd4995ec6e0024c46c100a71e6bd467e964
Amira.Burns <burnsal@rams.colostate.edu>
2022-09-30 16:47:44 -0600

    update model weights to load from Google drive

--- R/download_cache.R
+++ R/download_cache.R
@@ -1,13 +1,14 @@
 #' This function will download files necessary to run the model and store them in the package space
 #' 
-#' @param url location of file to download
+#' @param name location of file to download
 #' 
 #' @import rappdirs
 #' @import fs
+#' @import googledrive
 #' 
 #' @export
 #' 
-download_cache <- function(url = "https://www.dropbox.com/s/ap2pbut2eycibw0/weights_family_cpu.pth?dl=0")
+download_cache <- function(name = "weights_family_cpu.pth")
   {
 
   # set destination
@@ -15,14 +16,11 @@ download_cache <- function(url = "https://www.dropbox.com/s/ap2pbut2eycibw0/weig
   fs::dir_create(cache_path)
   
   # create a file path
-  path <- file.path(cache_path, gsub("\\?dl=0", "", fs::path_file(url)))
+  path <- file.path(cache_path, fs::path_file(name))
   
-  # download file if it doesn't already exist:
-  if (!file.exists(path)) {
-    cat(paste0("downloading ", fs::path_file(url)), " file\n")
-    utils::download.file(url, path, mode = "wb")
-  }
-
+  # download file; overwrite any existing version in case weights have been updated:
+  cat(paste0("downloading ", fs::path_file(name)), " file\n")
+  googledrive::drive_download(name, path=path, overwrite = TRUE)
 
   return(path)
 }
