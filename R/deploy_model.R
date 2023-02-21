835153147f51ef7b9595c563d6cd5fff4b58c93d
burnsal <amiraburns@gmail.com>
2023-02-21 14:02:37 -0700

    fix loop

--- R/deploy_model.R
+++ R/deploy_model.R
@@ -429,7 +429,7 @@ deploy_model <- function(
           if(write_bbox_csv){
             bbox_df <- write_bbox_df(predictions_list, w, h, bboxes)
             utils::write.csv(bbox_df, file.path(output_dir, paste(model_type, "predicted_bboxes.csv", sep="_")), row.names=FALSE)
-          
+          }
           # print update
           cat(paste0("\nResults saved for images 1 - ", i, "\n"))
         }
