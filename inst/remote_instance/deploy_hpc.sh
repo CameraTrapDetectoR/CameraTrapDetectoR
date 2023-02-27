439acd166684b993c5428a579ecf12200887ea74
burnsal <amiraburns@gmail.com>
2023-02-27 16:32:57 -0700

    add comments to bash script

--- inst/remote_instance/deploy_hpc.sh
+++ inst/remote_instance/deploy_hpc.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 #SBATCH --job-name='deployCTD'
-#SBATCH --partition='debug'
-#SBATCH --mail-user=Amira.Burns@usda.gov
+#SBATCH --partition='partition'
+#SBATCH --mail-user=First.Last@myemail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 module purge
 
 module load r/4.2.0
 
+# navigate to dir where your R script is stored
 cd /project/cameratrapdetector
 
+# run R script with user arguments specified
 Rscript deploy_model_hpc.R -d /90daydata/cameratrapdetector/minitrain -o /project/camertrapdetector/minitrain/ctd_deploy_out/ --sample50 TRUE
