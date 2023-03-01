fa39e8149d1b5c153e5a1c34807afe59bf92ed29
burnsal <amiraburns@gmail.com>
2023-02-28 21:36:04 -0700

    enhance sort_images function to allow for subdirectories within class by count

--- R/sort_images.R
+++ R/sort_images.R
@@ -13,12 +13,14 @@
 #' @param output_dir absolute path to directory in which to put sorted image folders
 #' @param review_threshold images with detections below this threshold will be moved into a folder
 #' titled "Manual_Review". Accepts values 0.01 - 1; default = NA.
+#' @param count_subdir boolean. Create subfolders inside each class directory for individual counts.
 #' @param remove_originals boolean. Delete original image files. Default = FALSE
 #' 
 #' 
 #' @export
 sort_images <- function(results = NULL, output_dir = NULL,
-                        review_threshold = NA, remove_originals = FALSE){
+                        review_threshold = NA, count_subdir = FALSE, 
+                        remove_originals = FALSE){
   
   # define output_dir; create it if it doesn't exist
   if(!dir.exists(output_dir)){
@@ -36,7 +38,20 @@ sort_images <- function(results = NULL, output_dir = NULL,
     dir.create(paste(output_dir, "manual_review", sep="/"))
   }
   
-  # create placeholder vector for new filepaths
+  # make subfolders for counts if based on user input
+  if(count_subdir){
+    for(i in 1:length(classes)){
+      # filter results to a given class
+      res_class <- results[results$class == class[i]]
+      # get unique counts for that class
+      counts <- sort(unique(res_class$count))
+      for(ct in 1:length(counts)){
+        dir.create(paste(output_dir, classes[i], counts[ct], sep="/"))
+      }
+    }
+  }
+  
+  # create placeholder vector for new file paths
   new_filename <- rep(NA, nrow(results))
   
   # loop through results and transfer files 
@@ -47,17 +62,27 @@ sort_images <- function(results = NULL, output_dir = NULL,
     old_loc <- pred$filename
     class <- pred$prediction
     conf <- pred$confidence_score
+    count <- pred$count
     
     # create new image name 
     img_id <- utils::tail(strsplit(old_loc, "\\\\")[[1]], n = 2)
     img_name <- paste(img_id[1], img_id[2], sep="_")
     
+    
     # create full path for new image loc/name
-    new_loc <- paste(output_dir, class, img_name, sep="/")
+    if(count_subdir){
+      new_loc <- paste(output_dir, class, count, img_name, sep="/")
+    }
+    else{
+      new_loc <- paste(output_dir, class, img_name, sep="/")
+    }
     
     # update destination based on user input for review_threshold
     if(!is.na(review_threshold)){
       if(conf < review_threshold){
+        if(count_subdir){
+          new_loc <- paste(output_dir, "manual_review", count, img_name, sep="/")
+        }
         new_loc <- paste(output_dir, "manual_review", img_name, sep="/")
       }
     }
