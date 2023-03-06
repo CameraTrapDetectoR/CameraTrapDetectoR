fcac9499ea4fae835767869f1fa200274544cd96
burnsal <amiraburns@gmail.com>
2023-03-06 12:23:24 -0700

    add warnings and code breaks to eval_model

--- R/eval_model.R
+++ R/eval_model.R
@@ -8,15 +8,51 @@
 #' 1 is the joined df with predictions and ground truths; item 2 
 #' 
 #' 
-#' @param results df of output from deploy_model. Currently only supports 'long' prediction format
-#' @param output_dir absolute path to directory in which to put sorted image folders
-#' @param review_threshold images with detections below this threshold will be moved into a folder
-#' titled "Manual_Review". Accepts values 0.01 - 1; default = NA.
-#' @param count_subdir boolean. Create subfolders inside each class directory for individual counts.
-#' @param remove_originals boolean. Delete original image files. Default = FALSE
-#' 
+#' @param preds df of output from deploy_model. Currently only supports 'long' prediction format
+#' @param data ground truth results. Must be an R dataframe and contain the following columns:
+#' @param filepath name of the column in `data` that contains absolute paths to the the images being assessed
+#' @param true_class name of the column in `data` that contains ground truth classifications. 
+#' Must match the class level of `preds` i.e. if `preds` contained predictions for the species model, this
+#' column must contain ground truth species. 
+#' @param assess_counts boolean. Evaluate how well the model predicts counts in addition to classes. Default = FALSE.
+#' @param true_count name of the column in `data` that contains ground truth counts for the class listed in 
+#' `true_class` for a given row.  
 #' 
 #' @export
-sort_images <- function(results = NULL, output_dir = NULL,
-                        review_threshold = NA, count_subdir = FALSE, 
-                        remove_originals = FALSE){
+eval_model <- function(preds = NULL, data = NULL,
+                       filepath = NULL, true_class = NULL, 
+                       assess_counts = FALSE, true_count = NULL,
+                       assess_seq = FALSE, seq_id = NULL){
+  
+  # warnings if receiving the wrong type of input:
+  if(!is.data.frame(preds)){
+    stop(paste0(preds, " must be in data.frame format. 
+                \nUse as.data.frame(", preds, ") to format your predictions.\n"))
+  }
+  
+  if(!is.data.frame(data)){
+    stop(paste0(data, " must be in data.frame format. 
+                \nUse as.data.frame(", data, ") to format your annotations.\n"))
+  }
+  
+  # warning if incorrect format for counts 
+  if(assess_counts & is.null(true_counts)){
+    stop(paste0("Please provide the column name for animal counts in ", data, 
+                " in the argument `true_count`\n"))
+  }
+  
+  # warning if incorrect format for counts 
+  if(assess_seq & is.null(seq_id)){
+    stop(paste0("Please provide the column name for sequence id in ", data, 
+                " in the argument `seq_id`\nTo define sequence info for your dataset,
+                see the function *assign_seq_metadata*\n"))
+  }
+  
+  # write function to :
+  # 1. join results to ground truths 
+  
+  # 2. calculate eval metrics  
+  # 3. output object containing joined df and evals
+  
+}
+  
