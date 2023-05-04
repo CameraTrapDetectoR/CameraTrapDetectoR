14fea48a4707f03b847b53983c364d7d84f7760b
AmiraBurns - USDA <Amira.Burns@usda.gov>
2023-05-04 09:50:23 -0600

    add model dictionary to package data

--- R/data.R
+++ R/data.R
@@ -32,3 +32,20 @@
 #'\item{\code}{count}{number of animals per \code{true_class} per image}
 #'}
 "labels"
+
+#'
+#'
+#'models
+#'
+#'list of all models available in the package
+#'
+#'@format ## `models`
+#'A data frame with 5 rows and 5 columns
+#'\describe{
+#'\item{\code{model_name}}{model and version name}
+#'\item{\code{model_type}}{model group: general, family, species, or pig_only}
+#'\item{\code{num_classes}}{number of class labels in that model (including background)}
+#'\item{\code{description}}{string description of model (under development)}
+#'\item{\code{URL}}{download link for model files}
+#'}
+"models"
