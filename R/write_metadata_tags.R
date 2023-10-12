0e15465a42c14a0d36fd66add233ff2b0da43124
AmiraBurns - USDA <Amira.Burns@usda.gov>
2023-10-12 12:10:43 -0600

    rename and outline write_metadata function

---
+++ R/write_metadata_tags.R
@@ -0,0 +1,23 @@
+#' Write predictions to image metadata
+#' 
+#' @description -UNDER DEVELOPMENT- write model outputs to image metadata
+#' 
+#' @param pred_df results for a given image
+#' @param model_version model version so we know which tags to write
+#' @param review_threshold review threshold for accepting predictions
+#' 
+#' @import exiftoolr
+#' 
+#' @export
+write_metadata_tags <- function(pred_df, model_version, review_threshold){
+  
+  # define config file
+  
+  # use model version to define tags
+  
+  # extract highest conf predictions with class count from pred_df
+  
+  # write metadata
+  
+
+}
